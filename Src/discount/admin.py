e5827996e6c63bf227e0eeec42704287c97ce00f
fghzamani <fgh.zamani72@gmail.com>
2021-08-26 23:44:40 +0430

    customized edited

--- Src/discount/admin.py
+++ Src/discount/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Discount
-# Register your models here.
 
-
-# admin.site.register(Discount)
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
     list_filter = ('active', )
